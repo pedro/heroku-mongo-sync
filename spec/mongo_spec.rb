ec9a0af741b26da284a96618bb2c7f59aac41e9a
Pedro Belo <pedro@heroku.com>
2010-02-25 01:00:53 -0800

    meh

--- spec/mongo_spec.rb
+++ spec/mongo_spec.rb
@@ -13,20 +13,18 @@ describe Heroku::Command::Mongo do
     @mongo.stubs(:app).returns('myapp')
   end
 
-  describe "URL parsing" do
-    it "rejects urls without host" do
-      @mongo.expects(:error)
-      @mongo.send(:make_uri, 'test')
-    end
+  it "rejects urls without host" do
+    @mongo.expects(:error)
+    @mongo.send(:make_uri, 'test')
+  end
 
-    it "rescues URI parsing errors" do
-      @mongo.expects(:error)
-      @mongo.send(:make_uri, 'test:')
-    end
+  it "rescues URI parsing errors" do
+    @mongo.expects(:error)
+    @mongo.send(:make_uri, 'test:')
+  end
 
-    it "fixes mongohq addresses so it can connect from outside EC2" do
-      uri = @mongo.send(:make_uri, 'mongo://root:secret@aws.mongohq.com/mydb')
-      uri.host.should == 'genesis.mongohq.com'
-    end
+  it "fixes mongohq addresses so it can connect from outside EC2" do
+    uri = @mongo.send(:make_uri, 'mongo://root:secret@aws.mongohq.com/mydb')
+    uri.host.should == 'genesis.mongohq.com'
   end
 end
