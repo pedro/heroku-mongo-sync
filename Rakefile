ab0642c84f5a65828757de031ac1705d7d07d61b
Pedro Belo <pedro@heroku.com>
2010-02-25 00:56:59 -0800

    first specs on URI parsing

--- Rakefile
+++ Rakefile
@@ -1,3 +1,9 @@
+desc 'Run specs'
+task :spec do
+  sh 'bacon -s spec/*_spec.rb'
+end
+task :default => :spec
+
 desc 'Install'
 task :install do
   folder = '~/.heroku/plugins/heroku-mongo-sync'
---
+++ spec/mongo_spec.rb
@@ -0,0 +1,32 @@
+require 'rubygems'
+require 'mongo'
+require 'baconmocha'
+
+$: << File.dirname(__FILE__) + '/../lib'
+require 'heroku'
+require 'heroku/command'
+require 'heroku/command/mongo'
+
+describe Heroku::Command::Mongo do
+  before do
+    @mongo = Heroku::Command::Mongo.new ['--app', 'myapp']
+    @mongo.stubs(:app).returns('myapp')
+  end
+
+  describe "URL parsing" do
+    it "rejects urls without host" do
+      @mongo.expects(:error)
+      @mongo.send(:make_uri, 'test')
+    end
+
+    it "rescues URI parsing errors" do
+      @mongo.expects(:error)
+      @mongo.send(:make_uri, 'test:')
+    end
+
+    it "fixes mongohq addresses so it can connect from outside EC2" do
+      uri = @mongo.send(:make_uri, 'mongo://root:secret@aws.mongohq.com/mydb')
+      uri.host.should == 'genesis.mongohq.com'
+    end
+  end
+end
