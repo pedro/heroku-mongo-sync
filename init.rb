878899260c37a412b80e613f2b04f63ac3b3f5f4
Pedro Belo <pedro@heroku.com>
2013-07-17 19:11:39 -0700

    also change init.rb in case someone installs this

--- init.rb
+++ init.rb
@@ -1,1 +1,3 @@
+$stderr.puts "the heroku-mongo-sync plugin has moved to a new owner"
+$stderr.puts "please refer to the fork at https://github.com/marcofognog/heroku-mongo-sync"
 require 'heroku/command/mongo'
